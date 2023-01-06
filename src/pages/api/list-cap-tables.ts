13b7218a24ee6e7f308d2c1667d8e15e95439795
Øyvind Hatland <oha@brreg.no>
2023-01-06 17:02:15 +0100

    /api/list-cap-table returns CapTableList

--- src/pages/api/list-cap-tables.ts
+++ src/pages/api/list-cap-tables.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { BRIDGE_CHAIN_CONFIG } from "../../constants";
+import { initSDK } from "../../components/brok-sdk";
 
 type Data = {
 	name: string;
@@ -12,5 +13,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 Legg til random historiske priser
  */
 
-	return res.status(200).json({ name: "John Doe" });
+	const sdk = await initSDK()
+
+	const capTables = await sdk.getCapTableList();
+
+	return res.status(200).json(capTables);
 }
