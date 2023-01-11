f8611787a1f4207c9920cd443504145011ed123e
Øyvind Hatland <oha@brreg.no>
2023-01-11 11:14:56 +0100

    list 999 first cap tables

--- src/pages/api/list-cap-table/index.ts
+++ src/pages/api/list-cap-table/index.ts
@@ -6,7 +6,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
 	const sdk = await initSDK()
 
-	const capTables = await sdk.getCapTableList();
+	const capTables = await sdk.getCapTableList(0,999);
 
 	return res.status(200).json(capTables);
 }
